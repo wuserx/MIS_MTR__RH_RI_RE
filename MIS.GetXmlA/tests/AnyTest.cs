7ed715cf34d5d6f227be1d360a6d1e514ef5466a
uart228 <help-mega@mail.ru>
2025-11-25 07:29:53 +0300

    refactoring

--- MIS.GetXmlA/tests/AnyTest.cs
+++
@@ -1,8 +0,0 @@
-﻿public class AnyTest
-{
-    public void Get()
-    {
-        var p1 = new RepositoryTest<MtrContext>().GetTestLEKPRSL();
-        var p2 = JsonConvert.DeserializeObject<List<SluchKoef>>("[{IDSL:1, Z_SL:1},{IDSL:1, Z_SL:1},{IDSL:1, Z_SL:1}]");
-    }
-}
